3db04c6d1982989f7074e6275a176edc279f0dd5
Matheus Mortatti <mamortat@microsoft.com>
2025-10-27 17:34:31 -0700

    add claude code to nvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Copy filename, line number and such
 vim.keymap.set("n", "<leader>ypf", function()
   vim.fn.setreg(vim.v.register, vim.fn.expand("%:p"))
 end, { desc = "Copy file path" })
@@ -9,3 +10,6 @@ end, { desc = "Copy file path" })
 vim.keymap.set("n", "<leader>ypl", function()
   vim.fn.setreg(vim.v.register, vim.fn.expand("%:p") .. ":" .. vim.fn.line("."))
 end, { desc = "Copy file path and cursor line number" })
+
+-- Claude Code
+-- vim.keymap.set("n", "<leader>cc", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
